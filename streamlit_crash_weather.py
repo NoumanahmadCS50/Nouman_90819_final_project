79ba0a4dc6cef5f677f7b083694be9cf41d4d931
Nouman Ahmad <62499603+NoumanahmadCS50@users.noreply.github.com>
2023-04-23 22:53:05 -0400

    Update streamlit_crash_weather.py

--- streamlit_crash_weather.py
+++ streamlit_crash_weather.py
@@ -1,10 +1,6 @@
-
-# Now you can safely import the required packages
-import seaborn as sns
-import matplotlib.pyplot as plt
+import altair as alt
 import pandas as pd
 import streamlit as st
-
 
 # Load data
 data = pd.read_csv("CrashData.csv")
@@ -18,7 +14,7 @@ st.write(data)
 
 st.write("Use the dropdown menu to select a specific month or reset to display all months. The visualizations will be updated accordingly.")
 
-# Create a dropdown menu for selecting the crash    `1 month
+# Create a dropdown menu for selecting the crash month
 crash_month = data['CRASH_MONTH'].unique().tolist()
 selected_category = st.selectbox("Select a month ", ["All"] + crash_month)
 
@@ -28,17 +24,19 @@ if selected_category == "All":
 else:
     filtered_data = data[data['CRASH_MONTH'] == selected_category]
 
-# Plot a bar chart of the crahses by months
-st.write("## Crashes by Months")
-crashes_by_months = filtered_data.groupby(['CRASH_MONTH']).size().reset_index(name='counts')
-fig, ax = plt.subplots()
-sns.barplot(x='CRASH_MONTH', y='counts', data=crashes_by_months, ax=ax, palette='viridis')
-plt.xticks(rotation=90)
-st.pyplot(fig)
+# Plot a bar chart of the crashes by month
+st.write("## Crashes by Month")
+crashes_by_month = filtered_data.groupby(['CRASH_MONTH']).size().reset_index(name='counts')
+chart_month = alt.Chart(crashes_by_month).mark_bar().encode(
+    x=alt.X('CRASH_MONTH', sort=None),
+    y='counts',
+    tooltip=['CRASH_MONTH', 'counts']
+).properties(width=600, height=400, title='Crashes by Month')
+st.altair_chart(chart_month)
 
 st.write("Use the dropdown menu to select a specific day or reset to display all days. The visualizations will be updated accordingly.")
 
-# Create a dropdown menu for selecting the crash    `1 day
+# Create a dropdown menu for selecting the crash day
 crash_day = data['DAY_OF_WEEK'].unique().tolist()
 selected_category = st.selectbox("Select a day ", ["All"] + crash_day)
 
@@ -51,16 +49,9 @@ else:
 # Plot a bar chart of the crashes by day
 st.write("## Crashes by Day")
 crashes_by_day = filtered_data.groupby(['DAY_OF_WEEK']).size().reset_index(name='counts')
-fig, ax = plt.subplots()
-sns.barplot(x='DAY_OF_WEEK', y='counts', data=crashes_by_day, ax=ax, palette='viridis')
-plt.xticks(rotation=90)
-st.pyplot(fig)
-
-# Plot a line chart of the monthly crime occurrences by average temperature
-st.write("## Monthly crashes by average temperature")
-monthly_crash = filtered_data.set_index('DATE').resample('M')['CRASH_MONTH'].count().reset_index(name='counts')
-monthly_weather = filtered_data.set_index('DATE').resample('M')['WEATHER'].mean().reset_index()
-monthly_weather['avg_temp'] = (monthly_weather['WEATHER'])
-monthly_data = pd.merge(monthly_crime, monthly_weather, on='CRASH_MONTH', how='outer')
-monthly_data = monthly_data.dropna()
-st.line_chart(monthly_data.set_index('CRASH_MONTH')[['counts', 'avg_temp']].rename(columns={'counts': 'Crash Occurrences', 'avg_temp': 'Avg. Temperature (C)'}), use_container_width=True)
+chart_day = alt.Chart(crashes_by_day).mark_bar().encode(
+    x=alt.X('DAY_OF_WEEK', sort=None),
+    y='counts',
+    tooltip=['DAY_OF_WEEK', 'counts']
+).properties(width=600, height=400, title='Crashes by Day')
+st.altair_chart(chart_day)
